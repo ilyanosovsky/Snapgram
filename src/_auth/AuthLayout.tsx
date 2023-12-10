a89565d93a820b6eb3051075fcd179e10bfc6291
ilyanosovsky <imnosovsky@gmail.com>
2023-12-10 21:05:28 +0200

    img pret

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -13,7 +13,7 @@ const AuthLayout = () => {
             <Outlet />
           </section>
 
-          <img 
+          <img
             src="/assets/images/side-img.svg"
             alt="logo"
             className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
