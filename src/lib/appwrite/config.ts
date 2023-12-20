14e547de21c501e7e57601ffc6027bf3ad853382
ilyanosovsky <imnosovsky@gmail.com>
2023-12-20 19:20:03 +0200

    home page

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -15,7 +15,6 @@ export const client = new Client();
 client.setProject(appwriteConfig.projectId);
 client.setEndpoint(appwriteConfig.url);
 
-
 export const account = new Account(client);
 export const databases = new Databases(client);
 export const storage = new Storage(client);
