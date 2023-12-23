6379c783b0ee6d62dd82f09acc8c3808efbb1781
ilyanosovsky <imnosovsky@gmail.com>
2023-12-23 15:51:04 +0200

    last changes

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -64,13 +64,13 @@ export const useGetPosts = () => {
       if (lastPage && lastPage.documents.length === 0) {
         return null;
       }
-    
+
       // Use the $id of the last document as the cursor.
       const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
       return lastId;
     },
     // Add an initialPageParam to satisfy TypeScript
-    initialPageParam: null, 
+    initialPageParam: null,
   });
 };
 
